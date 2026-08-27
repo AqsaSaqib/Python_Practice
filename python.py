dc7ee02de7d308cffbdf7ad62dc257cd84da6671
AqsaSaqib <92asaqib@gmail.com>
2026-08-27 21:36:59 +0500

    add new file

--- python.py
+++ python.py
@@ -13,3 +13,6 @@ print("Maximum of list is:\n", max(newlist))
 print("Mean value of list is:\n", sum(newlist)/len(newlist))
 
 print("Median value of list is:\n", newlist[len(newlist)//2] if len(newlist) % 2 == 1 else (newlist[len(newlist)//2 - 1] + newlist[len(newlist)//2]) / 2)
+
+mode = max(newlist.count)
+print("Mode of this list is:\n", mode)
